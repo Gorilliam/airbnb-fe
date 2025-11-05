a1964182166fc423a225a3024cae793d7c0800cc
Gorilliam <gorilliam99@icloud.com>
2025-11-05 12:29:36 +0100

    Add booking service helper for API

---
+++ src/utils/bookingService.ts
@@ -0,0 +1,62 @@
+class BookingService {
+  private baseUrl: string;
+  private bookingUrl: string;
+
+  constructor() {
+    this.baseUrl =
+      process.env.BACKEND_BASE_URL ||
+      process.env.NEXT_PUBLIC_API_URL ||
+      "";
+    this.bookingUrl = `${this.baseUrl}/booking`;
+  }
+
+  async getBookings(options: Partial<BookingListQuery> = {}) {
+    let url = `${this.bookingUrl}/`;
+    if (options.offset) url += `?offset=${options.offset}`;
+    return await fetch(url, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+  }
+
+  async getBooking(id: string) {
+    const url = `${this.bookingUrl}/${id}`;
+    return await fetch(url, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+  }
+
+  async createBooking(bookingData: Partial<Booking>) {
+    const url = `${this.bookingUrl}/`;
+    return await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(bookingData),
+    });
+  }
+
+  async updateBooking(id: string, bookingData: Partial<Booking>) {
+    const url = `${this.bookingUrl}/${id}`;
+    return await fetch(url, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(bookingData),
+    });
+  }
+
+  async deleteBooking(id: string) {
+    const url = `${this.bookingUrl}/${id}`;
+    return await fetch(url, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+  }
+}
+
+export default BookingService;
