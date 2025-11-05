7b0ecc74e5f2467a4f462c6d56c17722b9e0a160
Gorilliam <gorilliam99@icloud.com>
2025-11-05 23:15:45 +0100

    Add home page to sate vercel build critera

---
+++ src/app/home/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Welcome Home</h1>
+      <p>This is the home page for Airbnb Clone.</p>
+    </div>
+  );
+}
