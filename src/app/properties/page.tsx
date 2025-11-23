d927673b6aa4db33c0495aa5cf458baee427eb72
Gorilliam <gorilliam99@icloud.com>
2025-11-23 23:26:17 +0100

    removed the need for props

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -1,17 +1,12 @@
-import PropertyService from "@/utils/propertyService";
 import PropertyList from "@/components/properties/PropertyList";
 
-export default async function PropertiesPage() {
-  const response = await new PropertyService().getProperties();
-  const data: PaginatedListResponse<Property> = await response.json();
-
+export default function PropertiesPage() {
   return (
     <div className="p-10">
-      <h1 className="text-2xl font-bold mb-4">Properties</h1>
-
-      <PropertyList properties={data.data} />
+      <PropertyList />
     </div>
   );
 }
+
 
 
