adff682ce01287789837d18a63a20a5a35ffc030
Gorilliam <gorilliam99@icloud.com>
2025-11-23 23:51:13 +0100

    Remove link to add bookings from bookings

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -8,14 +8,6 @@ export default function BookingsPage() {
     <div className="p-10">
       <h1 className="text-2xl font-bold mb-4">Bookings</h1>
 
-      <Link
-        href="/bookings/new"
-        className="text-blue-600 underline font-medium"
-      >
-        Add New Booking
-      </Link>
-
-      {/* Den här komponenten hämtar och visar relationerna */}
       <BookingList />
     </div>
   );
