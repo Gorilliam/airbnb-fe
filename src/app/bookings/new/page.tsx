07732e90bd922740183cd60877d9875f61dfa09c
Gorilliam <gorilliam99@icloud.com>
2025-11-24 00:28:24 +0100

    Disable prerendering for /bookings/new (fix Vercel build)

--- src/app/bookings/new/page.tsx
+++ src/app/bookings/new/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useSearchParams } from "next/navigation";
 import BookingForm from "@/components/bookings/BookingForm";
 import BookingService from "@/utils/bookingService";
