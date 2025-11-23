c1b051977a78649a3fbafb63b586f46eecdab46e
Gorilliam <gorilliam99@icloud.com>
2025-11-24 00:35:45 +0100

    Fix to vercel build fail

--- src/app/bookings/new/page.tsx
+++ src/app/bookings/new/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 import { useSearchParams } from "next/navigation";
 import BookingForm from "@/components/bookings/BookingForm";
