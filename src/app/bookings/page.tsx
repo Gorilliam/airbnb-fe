5bf6f78da7c9fc5db23ce6139be3c503d75e03ab
Gorilliam <gorilliam99@icloud.com>
2025-11-23 17:20:07 +0100

    Change bookings page to render in BookingList

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -1,10 +1,9 @@
-import BookingService from "@/utils/bookingService";
+"use client";
+
+import BookingList from "@/components/bookings/BookingList";
 import Link from "next/link";
 
-export default async function BookingsPage() {
-  const response = await new BookingService().getBookings();
-  const data: PaginatedListResponse<Booking> = await response.json();
-
+export default function BookingsPage() {
   return (
     <div className="p-10">
       <h1 className="text-2xl font-bold mb-4">Bookings</h1>
@@ -16,30 +15,9 @@ export default async function BookingsPage() {
         Add New Booking
       </Link>
 
-      <ul className="mt-4 space-y-3">
-        {data.data.map((booking) => (
-          <li key={booking.id} className="border p-3 rounded shadow-sm">
-            <p>
-              <span className="font-semibold">Property:</span>{" "}
-              {booking.property_id}
-            </p>
-            <p>
-              <span className="font-semibold">User:</span> {booking.user_id}
-            </p>
-            <p>
-              {booking.check_in_date} → {booking.check_out_date}
-            </p>
-            <p className="text-gray-600">{booking.total_price} €</p>
-
-            <Link
-              href={`/bookings/${booking.id}/update`}
-              className="text-blue-600 text-sm underline"
-            >
-              Edit
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {/* Den här komponenten hämtar och visar relationerna */}
+      <BookingList />
     </div>
   );
 }
+
