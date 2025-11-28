a6c7cf04224cdbfa5c115694d6bdbd39a4287632
Gorilliam <gorilliam99@icloud.com>
2025-11-28 11:19:45 +0100

    Remove link to properties from /bookings

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 
 import BookingList from "@/components/bookings/BookingList";
-import Link from "next/link";
 
 export default function BookingsPage() {
   return (
     <div className="p-10">
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-2xl font-bold">Bookings</h1>
-        <Link
-          href="/properties"
-          className="text-blue-600 underline font-medium"
-        >
-          Properties
-        </Link>
+
       </div>
 
       <BookingList />
