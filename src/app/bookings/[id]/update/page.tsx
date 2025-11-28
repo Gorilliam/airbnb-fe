9c67354db1fd56cd3981791dccd4754e128040df
Gorilliam <gorilliam99@icloud.com>
2025-11-28 21:49:42 +0100

    Convert bookingswithrelations data to booking data

--- src/app/bookings/[id]/update/page.tsx
+++ src/app/bookings/[id]/update/page.tsx
@@ -2,17 +2,23 @@ import BookingService from "@/utils/bookingService";
 import UpdateBooking from "@/components/bookings/UpdateBooking";
 import { notFound } from "next/navigation";
 
-export default async function UpdateBookingPage({
-  params,
-}: PageProps<"/bookings/[id]/update">) {
-  const { id } = await params;
+export default async function UpdateBookingPage({ params }: { params: { id: string }}) {
+  const { id } = params;
+
   const response = await new BookingService().getBooking(id);
+  if (!response.ok) return notFound();
 
-  if (!response.ok) {
-    return notFound();
-  }
+  const data: BookingWithRelations = await response.json();
 
-  const booking: Booking = await response.json();
+  const booking: Booking = {
+    id: data.id,
+    property_id: data.property.id,
+    user_id: data.user.id,
+    check_in_date: data.check_in_date,
+    check_out_date: data.check_out_date,
+    total_price: data.total_price,
+    created_at: data.created_at,
+  };
 
   return <UpdateBooking booking={booking} />;
 }
