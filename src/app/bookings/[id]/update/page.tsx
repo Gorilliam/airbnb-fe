7e2861ba700be2cee8f5d1f13ddf4f80a0954a8d
Gorilliam <gorilliam99@icloud.com>
2025-11-28 22:53:24 +0100

    revert to simple logic

--- src/app/bookings/[id]/update/page.tsx
+++ src/app/bookings/[id]/update/page.tsx
@@ -2,23 +2,18 @@ import BookingService from "@/utils/bookingService";
 import UpdateBooking from "@/components/bookings/UpdateBooking";
 import { notFound } from "next/navigation";
 
-export default async function UpdateBookingPage({ params }: { params: { id: string }}) {
+export default async function UpdateBookingPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const { id } = params;
 
   const response = await new BookingService().getBooking(id);
+
   if (!response.ok) return notFound();
 
-  const data: BookingWithRelations = await response.json();
-
-  const booking: Booking = {
-    id: data.id,
-    property_id: data.property.id,
-    user_id: data.user.id,
-    check_in_date: data.check_in_date,
-    check_out_date: data.check_out_date,
-    total_price: data.total_price,
-    created_at: data.created_at,
-  };
+  const booking: Booking = await response.json();
 
   return <UpdateBooking booking={booking} />;
 }
