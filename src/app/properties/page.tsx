176585f8b855b1b745b6636c1cc5db55860157b2
Gorilliam <gorilliam99@icloud.com>
2025-11-28 11:15:58 +0100

    Remove old links now that we have navbar

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import PropertyList from "@/components/properties/PropertyList";
-import Link from "next/link";
-import { useUser } from "@/contexts/user";
 
 export default function PropertiesPage() {
-  const { user } = useUser();
 
   return (
     <div className="p-10">
@@ -13,27 +10,8 @@ export default function PropertiesPage() {
         <h1 className="text-2xl font-bold">Properties</h1>
 
         <div className="flex items-center gap-4">
-                  {/* Only hosts & admins can add properties */}
-          {(user?.role === "host" || user?.role === "admin") && (
-            <Link
-              href="/properties/new"
-              className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
-            >
-              + Add Property
-            </Link>
-          )}
-         
-          <Link
-            href="/bookings"
-            className="text-blue-600 underline font-medium"
-          >
-            My Bookings
-          </Link>
-
- 
         </div>
       </div>
-
       <PropertyList />
     </div>
   );
