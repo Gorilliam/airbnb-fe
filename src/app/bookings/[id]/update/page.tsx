37aa872d16fff06984e5652b1f39b2b72fef0aca
Gorilliam <gorilliam99@icloud.com>
2025-12-07 20:05:34 +0100

    Add troubleshhoting logs

--- src/app/bookings/[id]/update/page.tsx
+++ src/app/bookings/[id]/update/page.tsx
@@ -6,9 +6,14 @@ export default async function UpdateBookingPage({
   params,
 }: PageProps<"/bookings/[id]/update">) {
   const { id } = await params;
+  console.log("✏️ FETCHING BOOKING FOR UPDATE", id);
   const response = await new BookingService().getBooking(id);
 
+  console.log("✏️ STATUS FROM BE:", response.status);
+
   if (!response.ok) {
+    const text = await response.text();
+    console.log("✏️ ERROR BODY:", text);
     redirect("/bookings");
   }
 
