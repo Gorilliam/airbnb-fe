c78bb50f3f376d7bde47cfaf4ed5ae6664ddc3f6
Gorilliam <gorilliam99@icloud.com>
2025-12-07 01:33:46 +0100

    Remove home folder entirely since we do not use it

--- src/app/home/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div className="p-10">
-      <h1 className="text-2xl font-bold">Welcome Home</h1>
-      <p>This is the home page for Airbnb Clone.</p>
-    </div>
-  );
-}
